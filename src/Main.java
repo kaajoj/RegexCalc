2446a65bef5b588e0d5197cb18cc4d9a22d624ab
kaajoj <kaajooj@gmail.com>
2018-11-29 01:17:32 +0100

    added letters and = sign

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,12 @@ import java.util.regex.Pattern;
 
 public class Main {
 
-    public static double calc(String arg) {
-        Stack<Double> stack = new Stack<>();
-        String temp2 = "0";
+    public static int calc(String arg) {
+        Stack<Integer> stack = new Stack<>();
+        Map<String, Integer> map = new HashMap<>();
+
+        String ifLetter = "0";
+        String letter = "";
 
         for (String token : arg.split("\\s+")) {
             System.out.print(token);
@@ -18,11 +21,11 @@ public class Main {
             Matcher matcher = regex.matcher(text);
 
             while (matcher.find()) {
-                temp2 = matcher.group(0);
-                System.out.print(matcher.group(0) + " ");
+                ifLetter = matcher.group(0);
+                //System.out.print(matcher.group(0) + " ");
             }
 
-            if (temp2 == "0") {
+            if (ifLetter == "0") {
                 switch (token) {
                     case "=":
 
@@ -41,45 +44,33 @@ public class Main {
                         break;
                     case "/":
                         System.out.print("Div ");
-                        double temp = stack.pop();
+                        int temp = stack.pop();
                         stack.push(stack.pop() / temp);
-                        break;
-                    case "^":
-                        System.out.print("Pow ");
-                        double pow = stack.pop();
-                        stack.push(Math.pow(stack.pop(), pow));
                         break;
 
                     default:
                         System.out.print("Push ");
-                        stack.push(Double.parseDouble(token));
+                        stack.push(Integer.parseInt(token));
                         break;
                 }
                 System.out.println(stack);
             } else{
-                String tempVariable = temp2;
-                System.out.println(temp2);
+                letter = ifLetter;
+                ifLetter = "0";
+                System.out.println("");
             }
 
         }
-        double temp = stack.peek();
+        int temp = stack.peek();
+        map.put(letter, temp);
         System.out.println("Wynik: " + stack.pop());
+        System.out.println(map.get("d"));
         return temp;
     }
 
     public static void main(String[] args) {
-//        String text = args[0];
-//        //String pattern = "[a-zA-Z]+";
-//        String pattern = "=";
-//        Pattern regex = Pattern.compile(pattern);
-//        Matcher matcher = regex.matcher(text);
-//
-//        while(matcher.find()) {
-//            System.out.print(matcher.group(0)+" ");
-//        }
-//        System.out.println("");
 
-        double stack = 0;
+        int stack = 0;
         int i = 0;
 
         while(i<args.length) {
@@ -89,5 +80,7 @@ public class Main {
             System.out.println("");
             i++;
         }
+
+
     }
 }
